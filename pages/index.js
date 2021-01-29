de3aa0a1747834c6ec1c5aa8f4793fc6a8019d03
Felipe gomes <felipegomesboos@gmail.com>
2021-01-28 21:24:46 -0300

    adicionando arquivos da aula 02

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useRouter } from 'next/router';
 import db from '../db.json';
+
 import Widget from '../src/components/Widget';
 import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
@@ -26,6 +28,9 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
     <>
       <QuizBackground backgroundImage={db.bg}>
@@ -39,10 +44,23 @@ export default function Home() {
               </h1>
             </Widget.Header>
             <Widget.Content>
-              <p>
-                {db.description}
-                {' '}
-              </p>
+              <form onSubmit={function (InfoDoEvento) {
+                InfoDoEvento.preventDefault();
+                router.push(`/quiz?name=${name}`);
+                console.log('deu certo!');
+              }}
+              >
+                <input
+                  onChange={function (infoDoEvento) {
+                    setName(infoDoEvento.target.value);
+                  }}
+                  placeholder="Diz ai seu nick"
+                />
+                <button type="submit" disabled={name.length === 0}>
+                  Jogar
+                  {name}
+                </button>
+              </form>
             </Widget.Content>
           </Widget>
 
